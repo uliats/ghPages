87517a2aa8f9250c8d0a1deaf612a01939a4b1d4
uliats <117087988+uliats@users.noreply.github.com>
2024-02-24 14:14:17 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,6 @@
-import React from 'react';
-import { createRoot } from 'react-dom';
-import App from './App.tsx';
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
     <App />
-  </React.StrictMode>
-);
+)
